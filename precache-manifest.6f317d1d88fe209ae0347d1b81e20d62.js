5e4d614fd06c3c627cec9ad5b4c4321b154a7d41
Sudheer Singh <sudheersid0710@gmail.com>
2020-10-26 18:47:08 +0530

    Updates

---
+++ precache-manifest.6f317d1d88fe209ae0347d1b81e20d62.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92e926e65eb3ea4c963e15a39ca5c75",
+    "url": "/DSC-AKGEC/index.html"
+  },
+  {
+    "revision": "9689c4d9c137f84a9847",
+    "url": "/DSC-AKGEC/static/css/main.8dfb63d0.chunk.css"
+  },
+  {
+    "revision": "075bfbbc40b5cb70df52",
+    "url": "/DSC-AKGEC/static/js/2.92558802.chunk.js"
+  },
+  {
+    "revision": "fa0829a96ac07bb38fd94679172dd21a",
+    "url": "/DSC-AKGEC/static/js/2.92558802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9689c4d9c137f84a9847",
+    "url": "/DSC-AKGEC/static/js/main.d5acba21.chunk.js"
+  },
+  {
+    "revision": "8c1a72aea309e061fb24",
+    "url": "/DSC-AKGEC/static/js/runtime-main.fe93b4cb.js"
+  },
+  {
+    "revision": "07c120bc8b013d25e58fe45a2fe3a209",
+    "url": "/DSC-AKGEC/static/media/ANVESHANA.07c120bc.jpg"
+  },
+  {
+    "revision": "83ddbfb50e7f7a34dceca2ae8ab74075",
+    "url": "/DSC-AKGEC/static/media/Ambulon.83ddbfb5.jpg"
+  },
+  {
+    "revision": "61123d7a4a66e2a24152bd0a2ac37481",
+    "url": "/DSC-AKGEC/static/media/Dsc.61123d7a.png"
+  },
+  {
+    "revision": "9bc90c7e6f36b409cfaceff0f00e7763",
+    "url": "/DSC-AKGEC/static/media/ai-ml.9bc90c7e.png"
+  },
+  {
+    "revision": "58fe4faad51bdfd927b9f4d59ca857bf",
+    "url": "/DSC-AKGEC/static/media/app-d.58fe4faa.png"
+  },
+  {
+    "revision": "6120fe740c5e2192aec9c42c005ab511",
+    "url": "/DSC-AKGEC/static/media/background.6120fe74.PNG"
+  },
+  {
+    "revision": "b42d6b75d90781b8a66ba877e4b22570",
+    "url": "/DSC-AKGEC/static/media/bckTeam.b42d6b75.png"
+  },
+  {
+    "revision": "0d19cead1d00f0f6a15ca14821ac997b",
+    "url": "/DSC-AKGEC/static/media/card-back.0d19cead.jpg"
+  },
+  {
+    "revision": "eb5c26b3832b2f5674fbe9b01c0e62d3",
+    "url": "/DSC-AKGEC/static/media/cloud-platform.eb5c26b3.jpg"
+  },
+  {
+    "revision": "940d29918ced7e233f418c8cb3b68619",
+    "url": "/DSC-AKGEC/static/media/contactForm-img.940d2991.PNG"
+  },
+  {
+    "revision": "ffc9c52a89384cd06859de9af8ec8250",
+    "url": "/DSC-AKGEC/static/media/footer-bck.ffc9c52a.PNG"
+  },
+  {
+    "revision": "2c4935bb8296220cf86ff78e973fd45b",
+    "url": "/DSC-AKGEC/static/media/git.2c4935bb.png"
+  },
+  {
+    "revision": "d2f292ec22ff1290a194b73a47b44250",
+    "url": "/DSC-AKGEC/static/media/hihome.d2f292ec.jpg"
+  },
+  {
+    "revision": "2c1ec844102d2a597f0357a0e441ec24",
+    "url": "/DSC-AKGEC/static/media/manage.2c1ec844.png"
+  },
+  {
+    "revision": "0f3ab0f57194e40bb140e91d5a61d906",
+    "url": "/DSC-AKGEC/static/media/networking.0f3ab0f5.png"
+  },
+  {
+    "revision": "256514e69424d5943b8e5d1f7d8db5e3",
+    "url": "/DSC-AKGEC/static/media/pln-back.256514e6.PNG"
+  },
+  {
+    "revision": "93aeb1f055f495e94db0b868caa2dcec",
+    "url": "/DSC-AKGEC/static/media/port-project.93aeb1f0.jpg"
+  },
+  {
+    "revision": "c9dc4710c67b14d417f16043e0fdb7ae",
+    "url": "/DSC-AKGEC/static/media/progate.c9dc4710.jpg"
+  },
+  {
+    "revision": "8676f4d966173b127efba8a2e7c72431",
+    "url": "/DSC-AKGEC/static/media/startup-bck.8676f4d9.PNG"
+  },
+  {
+    "revision": "2afa2854b0b826d251e29e1388da7f90",
+    "url": "/DSC-AKGEC/static/media/training.2afa2854.png"
+  },
+  {
+    "revision": "7b1d27308c4b8b18dc027c906fd3a208",
+    "url": "/DSC-AKGEC/static/media/web-d.7b1d2730.png"
+  }
+]);
